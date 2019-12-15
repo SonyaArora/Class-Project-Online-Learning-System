d805bc40b78f39f34ed2f58c7a46e8bb3cef47bd
SonyaArora <58151711+SonyaArora@users.noreply.github.com>
2019-12-15 17:46:33 -0500

    Well see...
    ALRIGHT

--- src/TopicForm.js
+++ src/TopicForm.js
@@ -1,34 +1,38 @@
-import React, { Component } from 'react';
-import "./App.css"
+import React, { Component } from "react";
+import "./App.css";
 
 class TopicForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: '*'};
+    this.state = { value: "*" };
     this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({ value: event.target.value });
     this.props.submitHandler(event.target.value);
   }
 
   render() {
     const labelStyle = {
-      "fontSize": "32px",
-      color: "white",
-      textAlign: "center",
+      fontSize: "32px",
+      color: "black",
+      textAlign: "center"
     };
 
     const selectStyle = {
-      "fontSize": "20px",
+      fontSize: "20px"
     };
 
     return (
       <div className="topic-selector">
         <label style={labelStyle}>
           Browse course listings by topic:&nbsp;
-          <select style={selectStyle} value={this.state.value} onChange={this.handleChange}>
+          <select
+            style={selectStyle}
+            value={this.state.value}
+            onChange={this.handleChange}
+          >
             <option value="">Select a topic</option>
             <option value="art">Art</option>
             <option value="math">Math</option>
